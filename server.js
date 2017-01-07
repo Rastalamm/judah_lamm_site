dd3e1b286791005e7f51836c10da59259c7d3c8d
Judah Lamm <Judah.Lamm@gmail.com>
2017-01-06 19:27:37 -0600

    Unindent file and add a router file

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
 "use strict";
-    const express = require('express');
-    const app = express();
-    const bodyParser = require('body-parser');
-    const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const routes = require('./routes/index.js')
+const helmet = require('helmet')
 
-    let server;
+let server;
 
-    app.set('view engine', 'pug');
-    app.set('views', path.join(__dirname, '/views'));
-    app.use(express.static('public'));
-    app.use('/images', express.static(__dirname + '/images'));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static('public'));
+app.use('/images', express.static(__dirname + '/images'));
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
-        extended: false
-    }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-    app.use('/', (ignore, res) => {res.status(200).render("index.pug")});
+app.use(helmet())
 
-    app.use(function (ignore, res) {
-        res.status(404).render("404/index.pug");
-    });
+app.use('/', routes);
 
-    server = app.listen(process.env.app_port || 3000, function () {
-        const host = server.address().address;
-        const port = server.address().port;
+app.use(function (ignore, res) {
+    res.status(404).render("404/index.pug");
+});
 
-        console.log('Listening at http://%s:%s', host, port);
-    });
+server = app.listen(process.env.app_port || 3000, function () {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log('Listening at http://%s:%s', host, port);
+});
